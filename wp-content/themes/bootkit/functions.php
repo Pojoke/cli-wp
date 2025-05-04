302e491cf9bb3ae0cd456a478a3bad3653886026
Pojoke <pavlykdmutro@gmail.com>
2025-05-04 13:33:24 +0300

    dzz

---
+++ wp-content/themes/bootkit/functions.php
@@ -0,0 +1,8 @@
+<?php
+
+// Setup
+// Includes
+include get_theme_file_path('includes/enqueue.php');
+// Hooks
+add_action('wp_enqueue_scripts', 'bootkit_enqueue');
+// Shortcodes
---
+++ wp-content/themes/bootkit/includes/enqueue.php
@@ -0,0 +1,14 @@
+<?php
+echo 'Hello';
+function bootkit_enqueue()
+{
+    //echo 'Hello';
+   $url = get_theme_file_uri();
+    echo $url . '/assets/bootstrap/bootstrap.min.css';
+    wp_register_style('bootkit_google_fonts', 'https://fonts.googleapis.com/css?family=Open+Sans&display=swap'); // Google fonts
+    wp_register_style('bootkit_bootstrap', $url . '/assets/bootstrap/css/bootstrap.min.css'); // bootstrap.min.css
+    wp_register_style('bootkit_modern_business', $url . '/assets/css/styles.css'); // modern-business.css
+    wp_enqueue_style('bootkit_google_fonts');
+    wp_enqueue_style('bootkit_bootstrap');
+    wp_enqueue_style('bootkit_modern_business');
+}
