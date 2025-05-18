491fd6a8fd3ec9863c982055a99c807b10409ead
Pojoke <pavlykdmutro@gmail.com>
2025-05-18 09:12:41 +0300

    dz

--- wp-content/themes/bootkit/single.php
+++ wp-content/themes/bootkit/single.php
@@ -1,166 +1,98 @@
-
 <?php
 get_header(); ?>
 
-
-
-            <!-- Features section-->
-            <section class="py-5" id="features">
-                <div class="container px-5 my-5">
-                    <div class="row gx-5">
-                        <div class="col-lg-4 mb-5 mb-lg-0"><h2 class="fw-bolder mb-0">A better way to start building.</h2></div>
-                        <div class="col-lg-8">
-                            <div class="row gx-5 row-cols-1 row-cols-md-2">
-                                <div class="col mb-5 h-100">
-                                    <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-collection"></i></div>
-                                    <h2 class="h5">Featured title</h2>
-                                    <p class="mb-0">Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
-                                </div>
-                                <div class="col mb-5 h-100">
-                                    <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-building"></i></div>
-                                    <h2 class="h5">Featured title</h2>
-                                    <p class="mb-0">Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
-                                </div>
-                                <div class="col mb-5 mb-md-0 h-100">
-                                    <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-toggles2"></i></div>
-                                    <h2 class="h5">Featured title</h2>
-                                    <p class="mb-0">Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
-                                </div>
-                                <div class="col h-100">
-                                    <div class="feature bg-primary bg-gradient text-white rounded-3 mb-3"><i class="bi bi-toggles2"></i></div>
-                                    <h2 class="h5">Featured title</h2>
-                                    <p class="mb-0">Paragraph of text beneath the heading to explain the heading. Here is just a bit more text.</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            <!-- Testimonial section-->
-            <div class="py-5 bg-light">
-                <div class="container px-5 my-5">
-                    <div class="row gx-5 justify-content-center">
-                        <div class="col-lg-10 col-xl-7">
-                            <div class="text-center">
-                                <div class="fs-4 mb-4 fst-italic">"Working with Start Bootstrap templates has saved me tons of development time when building new projects! Starting with a Bootstrap template just makes things easier!"</div>
-                                <div class="d-flex align-items-center justify-content-center">
-                                    <img class="rounded-circle me-3" src="https://dummyimage.com/40x40/ced4da/6c757d" alt="..." />
-                                    <div class="fw-bold">
-                                        Tom Ato
-                                        <span class="fw-bold text-primary mx-1">/</span>
-                                        CEO, Pomodoro
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-           <!-- Page Content -->
+<!-- Page Content -->
 <div class="container">
 
-    
+
+
     <!-- Marketing Icons Section -->
     <div class="row">
-       
-       <!-- Post Content Column -->
-      <div class="col-lg-8">
-        <?php if (have_posts()) {
-    while (have_posts()) {
-        the_post();
-         global $post;
-          $author_ID = $post->post_author;
-        $author_URL = get_author_posts_url($author_ID);
-        ?>
-        
-        <!-- Title -->
-        <h1 class="mt-4"><?php the_title()?></h1>
 
-        <!-- Author -->
-         <p class="lead">
+        <!-- Post Content Column -->
+        <div class="col-lg-8">
+            <?php if (have_posts()) {
+                    while (have_posts()) {
+                        the_post();
+                        global $post;
+                        $author_ID  = $post->post_author;
+                        $author_URL = get_author_posts_url($author_ID);
+                    ?>
+            <!-- Title -->
+            <h1 class="mt-4"><?php the_title()?></h1>
+            <!-- Post category: -->
+            <h2 class="mt-4"><?php the_category(" ")?></h2>
+            <!-- Author -->
+            <p class="lead">
                 by
-                <a href="<?php echo $author_URL; ?>"><?php the_author();?></a>
+                <a href="<?php echo $author_URL; ?>"><?php the_author(); ?></a>
             </p>
-        <hr>
-        <!-- Date/Time -->
-        <p><?php echo get_the_date() ?></p>
-        <hr>
-        <!-- Preview Image -->
-          <?php
-if (has_post_thumbnail()) {
-            the_post_thumbnail("full", ["class" => "card-img-top"]);
-        }
-        ?>
-        <hr>
-        <!-- Post Content -->
-       <?php the_content();
-        $defaults = array(
-            'before' => '<div class="row justify-content-center align-items-center">' . __('Pages:'),
-            'after' => '</div>',
-        );
-        wp_link_pages($defaults);
-         edit_post_link();
-       ?>
-        <hr>
-<!-- Tag cloud -->
-            <?php the_tags();?>
             <hr>
-        <!-- Comments Form -->
-
-        <div class="card my-4">
-          <h5 class="card-header">Leave a Comment:</h5>
-          <div class="card-body">
-            <form>
-              <div class="form-group">
-                <textarea class="form-control" rows="3"></textarea>
-              </div>
-              <button type="submit" class="btn btn-primary">Submit</button>
-            </form>
-          </div>
-        </div>
-        <!-- Single Comment -->
-        <div class="media mb-4">
-          <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
-          <div class="media-body">
-            <h5 class="mt-0">Commenter Name</h5>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-          </div>
-        </div>
-
-        <!-- Comment with nested comments -->
-        <div class="media mb-4">
-          <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
-          <div class="media-body">
-            <h5 class="mt-0">Commenter Name</h5>
-            Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-            <div class="media mt-4">
-              <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
-              <div class="media-body">
-                <h5 class="mt-0">Commenter Name</h5>
-                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-              </div>
-            </div>
-            <div class="media mt-4">
-              <img class="d-flex mr-3 rounded-circle" src="http://placehold.it/50x50" alt="">
-              <div class="media-body">
-                <h5 class="mt-0">Commenter Name</h5>
-                Cras sit amet nibh libero, in gravida nulla. Nulla vel metus scelerisque ante sollicitudin. Cras purus odio, vestibulum in vulputate at, tempus viverra turpis. Fusce condimentum nunc ac nisi vulputate fringilla. Donec lacinia congue felis in faucibus.
-              </div>
-            </div>
-          </div>
+            <!-- Date/Time -->
+            <p><?php the_time(get_option('date_format'));
+                       echo " ";
+                       the_time(get_option('time_format')); ?></p>
+            <hr>
+            <!-- Preview Image -->
             <?php
-}
-}?>
+                if (has_post_thumbnail()) {
+                            the_post_thumbnail("full", ["class" => "card-img-top"]);
+                        }
+                    ?>
+            <hr>
+            <!-- Post Content -->
+            <?php the_content();
+                        $defaults = [
+                            'before' => '<div class="row justify-content-center align-items-center">' . __('Pages:'),
+                            'after'  => '</div>',
+                        ];
+                        wp_link_pages($defaults);
+                        edit_post_link();
+                    ?>
+            <!-- Post Author Info -->
+            <div class="card">
+                <div class="card-header">
+                    <strong>
+                        Posted by
+                        <a href="<?php echo $author_URL; ?>"><?php the_author();?></a>
+                    </strong>
+                </div>
+                <div class="card-body">
+                    <div class="author-image">
+                        <?php echo get_avatar($author_ID, 90, '', false, ['class' => 'img-circle']); ?>
+                    </div>
+                    <?php echo nl2br(get_the_author_meta('description')); ?>
+                </div>
+            </div><!-- Post Single - Author End -->
+            <hr>
+
+            <!-- Tag cloud -->
+            <?php the_tags('', ', '); ?>
+            <hr>
+
+            <!-- Pagination -->
+            <ul class="pagination justify-content-center mb-4">
+                <li class="page-item">
+                    <?php previous_post_link(); ?>
+                </li>
+                <li class="page-item">
+                    <?php next_post_link(); ?>
+                </li>
+            </ul>
+            <?php
+                if (comments_open() || get_comments_number()) {
+                            comments_template();
+                        }                   
+                }
+            }?>
         </div>
-        <?php get_sidebar();?>
-        <!-- /.row -->
+        <?php get_sidebar(); ?>
+        <?php /* get_sidebar('second')  sidebar-second.php */?>
     </div>
-    <!-- /.container -->
-        </div>
-      </div>
-      
-            </section>
-           
-        </main>
-        
-        <?php get_footer(); ?>
+    <!-- /.row -->
+
+
+</div>
+<!-- /.container -->
+
+<?php get_footer(); ?>
